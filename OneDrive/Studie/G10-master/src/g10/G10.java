5198ee6e9bc923adf08f1a2119ec85452fc99efb
jonab <jonab@10.50.130.110>
2018-05-01 14:11:24 +0200

    1/5 testtesttest

---
+++ OneDrive/Studie/G10-master/src/g10/G10.java
@@ -0,0 +1,29 @@
+package g10;
+
+import battleship.interfaces.BattleshipsPlayer;
+import tournament.player.PlayerFactory;
+
+public class G10 implements PlayerFactory<BattleshipsPlayer> {
+
+    @Override
+    public BattleshipsPlayer getNewInstance() {
+        return new Player();
+    }
+
+    @Override
+    public String getID() {
+        return "G10";
+    }
+
+    @Override
+    public String getName() {
+        return ""; //insert cool name
+    }
+
+    @Override
+    public String[] getAuthors() {
+        String[] authors = {"Jonatan", "Mads"};
+        return authors;
+    }
+
+}
